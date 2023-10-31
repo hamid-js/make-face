79a5fe1d0e9b9fcda5c8766230b9c84840a03227
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2023-10-31 22:15:21 +0330

    edit folder  stacture

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const monsterTail = document.querySelector(".monster-tail");
 const monsterHorns = document.querySelector(".monster-horns");
 const monsterBody = document.querySelector(".monster-body");
@@ -12,93 +11,66 @@ const faceBtn = document.getElementById("face-btn");
 
 let counter = 0;
 
+let colors = ["#a8d530", "#42aaff", "#f3d55b", "#ff4f51", "#904ae8", "#ffa711"];
 
-let colors = [
-    "#a8d530",
-    "#42aaff",
-    "#f3d55b",
-    "#ff4f51",
-    "#904ae8",
-    "#ffa711",
-   
-
-]
-
-let colors2 = [ ...colors, "transparent" ]
-
+let colors2 = [...colors, "transparent"];
 
 let images = [
-    "./face-0.png",
-    "./face-1.png",
-    "./face-2.png",
-    "./face-3.png",
-    "./face-4.png",
-    "./face-5.png",
-    
-
-]
-
+  "images/face-0.png",
+  "images/face-1.png",
+  "images/face-2.png",
+  "images/face-3.png",
+  "images/face-4.png",
+  "images/face-5.png",
+];
 
 const faceBtnMaker = () => {
-    
-   if( counter >= images.length -1 ){
+  if (counter >= images.length - 1) {
     counter = -1;
-   }{
-    counter++
-   
-   }
-   
-    monsterFace.src= images[counter]
-}
+  }
+  {
+    counter++;
+  }
+
+  monsterFace.src = images[counter];
+};
 const colorBtnMaker = () => {
-    
-    if( counter >= colors.length -1 ){
-     counter = -1;
-    }{
-     counter++
-    
-    }
-    
-    
-    myRoot.style.setProperty("--color-character", colors[counter] ) 
-}
+  if (counter >= colors.length - 1) {
+    counter = -1;
+  }
+  {
+    counter++;
+  }
+
+  myRoot.style.setProperty("--color-character", colors[counter]);
+};
 
 const tailBtnMaker = () => {
-    if( counter >= colors2.length -1 ){
-        counter = -1;
-       }{
-        counter++
-       
-       }
-       
-       
-       myRoot.style.setProperty("--color-tail", colors2[counter] ) 
-}
+  if (counter >= colors2.length - 1) {
+    counter = -1;
+  }
+  {
+    counter++;
+  }
 
+  myRoot.style.setProperty("--color-tail", colors2[counter]);
+};
 
 const hornsBtnMaker = () => {
-    if( counter >= colors2.length -1 ){
-        counter = -1;
-       }{
-        counter++
-       
-       }
-       
-       
-       myRoot.style.setProperty("--color-horns", colors2[counter] ) 
-}
+  if (counter >= colors2.length - 1) {
+    counter = -1;
+  }
+  {
+    counter++;
+  }
 
+  myRoot.style.setProperty("--color-horns", colors2[counter]);
+};
 
+tailBtn.addEventListener("click", tailBtnMaker);
 
-tailBtn.addEventListener("click",tailBtnMaker);
+hornsBtn.addEventListener("click", hornsBtnMaker);
 
-hornsBtn.addEventListener("click",hornsBtnMaker);
+colorBtn.addEventListener("click", colorBtnMaker);
 
-colorBtn.addEventListener("click",colorBtnMaker);
-
-faceBtn.addEventListener("click",faceBtnMaker);
-
-
-
-
-
+faceBtn.addEventListener("click", faceBtnMaker);
